468ebe8e86be33af01415e7f9dda7738436af125
Marylinemds <mendes.escom@gmail.com>
2017-03-27 23:19:36 +0200

    DELETE function of the content provider

--- app/src/main/java/com/example/android/popularmovies1/data/MoviesContentProvider.java
+++ app/src/main/java/com/example/android/popularmovies1/data/MoviesContentProvider.java
@@ -137,8 +137,35 @@ public class MoviesContentProvider extends ContentProvider {
     }
 
     @Override
-    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mMoviesDbHelper.getWritableDatabase();
+
+        int match = sUriMatcher.match(uri);
+        // Keep track of the number of deleted tasks
+        int favoriteDeleted; // starts as 0
+
+        // COMPLETED (2) Write the code to delete a single row of data
+        // [Hint] Use selections to delete an item by its row ID
+        switch (match) {
+            // Handle the single item case, recognized by the ID included in the URI path
+            case FAVORITE_WITH_ID:
+                // Get the task ID from the URI path
+                String id = uri.getPathSegments().get(1);
+                // Use selections/selectionArgs to filter for this ID
+                favoriteDeleted = db.delete(TABLE_NAME, "_id=?", new String[]{id});
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        // COMPLETED (3) Notify the resolver of a change and return the number of items deleted
+        if (favoriteDeleted != 0) {
+            // A task was deleted, set notification
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        // Return the number of tasks deleted
+        return favoriteDeleted;
     }
 
     @Override
